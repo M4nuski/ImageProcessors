cf849b79cf41fb36b384b56ea81db6bdfdac6cbe
M4nuski <the_man994@hotmail.com>
2015-03-15 19:17:36 -0400

    Added Regions and code cleaning

--- SaveAsFIT/ImageControl.cs
+++ SaveAsFIT/ImageControl.cs
@@ -7,6 +7,8 @@ namespace SaveAsFIT
 
     public partial class ImageControl : UserControl
     {
+    #region Properties
+        public Point PanPosition;
 
         private float zoomLevel;
         public float ZoomLevel
@@ -37,9 +39,9 @@ namespace SaveAsFIT
                 }
             }
         }
+
         private float fitZoomLevel; //also hapens to be minimum zoom level
         private float maxZoomLevel = 10.0f;
-
         public float MaxZoomLevel
         {
             get
@@ -59,16 +61,71 @@ namespace SaveAsFIT
         private int maxPanX, maxPanY;
         private int minPanX, minPanY;
         private bool panning;
-
-
-        public Point PanPosition;
-
+        #endregion 
+        
+        #region UI Methods
         public ImageControl()
         {
             InitializeComponent();
             MouseWheel += ImageControl_MouseWheel;
         }
 
+        private void ImageControl_Paint(object sender, PaintEventArgs e)
+        {
+            if (SourceImage != null)
+            {
+                e.Graphics.DrawImage(SourceImage, DisplayRectangle, getCurrentRectangle(), GraphicsUnit.Pixel);
+            }
+        }
+
+        private void ImageControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            panning = true;
+            lastMouseX = e.X;
+            lastMouseY = e.Y;
+        }
+
+        private void ImageControl_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0)
+            {
+                ZoomLevel = Math.Min(maxZoomLevel, ZoomLevel * 1.5f);
+            }
+            else if (e.Delta < 0)
+            {
+                ZoomLevel = Math.Max(fitZoomLevel, ZoomLevel / 1.5f);
+            }
+        }
+
+        private void ImageControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            panning = false;
+        }
+
+        private void ImageControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (panning)
+            {
+                var tempX = clamp(PanPosition.X + lastMouseX - e.X, minPanX, maxPanX);
+                var tempY = clamp(PanPosition.Y + lastMouseY - e.Y, minPanY, maxPanY);
+
+                PanPosition = new Point(tempX, tempY);
+
+                lastMouseX = e.X;
+                lastMouseY = e.Y;
+                Refresh();
+            }
+        }
+
+        private void ImageControl_Resize(object sender, EventArgs e)
+        {
+            var lastFitZoomLevel = fitZoomLevel;
+            setFitZoomLevel();
+            ZoomLevel = zoomLevel*fitZoomLevel/lastFitZoomLevel;
+        }
+        #endregion
+       
+        #region Control Methods
         private void setZoomLevel(float newLevel)
         {
             if (sourceImage != null)
@@ -131,78 +188,19 @@ namespace SaveAsFIT
             ZoomLevel = fitZoomLevel;
         }
 
-        private void ImageControl_Paint(object sender, PaintEventArgs e)
-        {
-            if (SourceImage != null)
-            {
-                e.Graphics.DrawImage(SourceImage, DisplayRectangle, getCurrentRectangle(), GraphicsUnit.Pixel);
-            }
-        }
-
-        private void ImageControl_MouseDown(object sender, MouseEventArgs e)
-        {
-            panning = true;
-            lastMouseX = e.X;
-            lastMouseY = e.Y;
-        }
-
-        private void ImageControl_MouseWheel(object sender, MouseEventArgs e)
-        {
-            if (e.Delta > 0)
-            {
-                ZoomLevel = Math.Min(maxZoomLevel, ZoomLevel * 1.5f);
-            }
-            else if (e.Delta < 0)
-            {
-                ZoomLevel = Math.Max(fitZoomLevel, ZoomLevel / 1.5f);
-            }
-        }
-
-        private void ImageControl_MouseUp(object sender, MouseEventArgs e)
-        {
-            panning = false;
-        }
-
-        private void ImageControl_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (panning)
-            {
-                var tempX = clamp(PanPosition.X + lastMouseX - e.X, minPanX, maxPanX);
-                var tempY = clamp(PanPosition.Y + lastMouseY - e.Y, minPanY, maxPanY);
-
-                PanPosition = new Point(tempX, tempY);
-
-                lastMouseX = e.X;
-                lastMouseY = e.Y;
-                Refresh();
-            }
-        }
-
-        private void ImageControl_Resize(object sender, EventArgs e)
-        {
-            var lastFitZoomLevel = fitZoomLevel;
-            setFitZoomLevel();
-            ZoomLevel = zoomLevel*fitZoomLevel/lastFitZoomLevel;
-        }
-
         private Rectangle getCurrentRectangle()
         {
-            //    var l = (PanPosition.X / zoomLevel) + srcCenterX - (destCenterX / ZoomLevel);
-            //    var t = (PanPosition.Y / zoomLevel) + srcCenterY - (destCenterY / ZoomLevel);
             var l = PanPosition.X / zoomLevel;
             var t = PanPosition.Y / zoomLevel;
 
             var w = Width / zoomLevel;
             var h = Height / zoomLevel;
 
-            //      if (t < 0) t = 0;
-            //      if (l < 0) l = 0;
-            //if ((l+w) > sourceImage.Width) w = sourceImage.Width-l;
-            //if ((t+h) > sourceImage.Height) h = sourceImage.Height-t;
-
             return new Rectangle((int)l, (int)t, (int)w, (int)h);
         }
+        #endregion
 
+        #region Helper Methods
         private static int clamp(int val, int min, int max)
         {
             if (val > max) val = max;
@@ -216,8 +214,6 @@ namespace SaveAsFIT
             if (val < min) val = min;
             return val;            
         }
-
+        #endregion
     }
-
-
 }
