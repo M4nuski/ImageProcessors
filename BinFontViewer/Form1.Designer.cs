ac0ea7b5b815b7d066e3a74d76bc7b8d6f34b133
Emmanuel Charette <the_man994@hotmail.com>
2025-04-19 13:35:20 -0400

    added BIN font viewer project

---
+++ BinFontViewer/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace BinFontViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_load = new System.Windows.Forms.Button();
+            this.textBox_w = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(12, 12);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(75, 23);
+            this.button_load.TabIndex = 0;
+            this.button_load.Text = "Load";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.button_load_Click);
+            // 
+            // textBox_w
+            // 
+            this.textBox_w.Location = new System.Drawing.Point(93, 12);
+            this.textBox_w.Name = "textBox_w";
+            this.textBox_w.Size = new System.Drawing.Size(100, 22);
+            this.textBox_w.TabIndex = 1;
+            this.textBox_w.Text = "8";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "bin";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "All|*.*";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(19, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1024, 512);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 663);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox_w);
+            this.Controls.Add(this.button_load);
+            this.Name = "Form1";
+            this.Text = "Bin font Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.TextBox textBox_w;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ BinFontViewer/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace BinFontViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_load_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            var data = File.ReadAllBytes(openFileDialog1.FileName);
+
+            if (data.Length == 0) return;
+
+            var BBruh = new SolidBrush(Color.Black);
+            var WBruh = new SolidBrush(Color.White);
+
+            var d_w = int.Parse(textBox_w.Text);
+            if (d_w < 0) d_w = 1;
+            var bp = new Bitmap(1024, 512);
+            var row = 0;
+            var col = 0;
+            var g = Graphics.FromImage(bp);
+            for (var i = 0; i < data.Length; i++)
+            {
+                g.FillRectangle(((data[i] & 0x80) == 0) ? BBruh : WBruh, (col + 0) * 2, row * 2, 2, 2);
+                g.FillRectangle(((data[i] & 0x40) == 0) ? BBruh : WBruh, (col + 1) * 2, row * 2, 2, 2);
+                g.FillRectangle(((data[i] & 0x20) == 0) ? BBruh : WBruh, (col + 2) * 2, row * 2, 2, 2);
+                g.FillRectangle(((data[i] & 0x10) == 0) ? BBruh : WBruh, (col + 3) * 2, row * 2, 2, 2);
+
+                g.FillRectangle(((data[i] & 0x08) == 0) ? BBruh : WBruh, (col + 4) * 2, row * 2, 2, 2);
+                g.FillRectangle(((data[i] & 0x04) == 0) ? BBruh : WBruh, (col + 5) * 2, row * 2, 2, 2);
+                g.FillRectangle(((data[i] & 0x02) == 0) ? BBruh : WBruh, (col + 6) * 2, row * 2, 2, 2);
+                g.FillRectangle(((data[i] & 0x01) == 0) ? BBruh : WBruh, (col + 7) * 2, row * 2, 2, 2);
+
+                row++;
+                if (row >= 256)
+                {
+                    row = 0;
+                    col += d_w;
+                }
+            }
+            pictureBox1.Image = bp;
+            pictureBox1.Refresh();
+
+        }
+    }
+}
